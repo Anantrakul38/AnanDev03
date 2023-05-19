410af320b8ef48132e445260df8f3c444529cdbb
anandev2023 <anantrakul38@gmail.com>
2023-05-19 13:21:43 +0700

    test06

--- js/main.js
+++ js/main.js
@@ -48,5 +48,78 @@ var swiper = new Swiper(".home-slider", {
     autoplay: {
         delay: 5000,
         disableOnInteraction: false,
+        pauseOnMouseEnter: true,	
       },
+  });
+
+/* section Reviews-slider */
+var swiper = new Swiper(".reviews-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+    },
+     autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,	
+      }, 
+  });
+
+/* section blogs-slider */
+var swiper = new Swiper(".blogs-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+    },
+     autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,	
+      }, 
+  });
+
+/* section blogs-slider */
+var swiper = new Swiper(".logo-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    breakpoints: {
+      450: {
+        slidesPerView: 2,
+      },
+      640: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      1000: {
+        slidesPerView: 5,
+      },
+    },
+     autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,	
+      }, 
   });
